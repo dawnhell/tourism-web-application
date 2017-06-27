7eb38c2a90f2e72619cfcdbec4cde06c196c68d9
Pavel Razuvalov <pavel@razuvalov.com>
2017-06-27 20:14:07 +0300

    Sidebar deleted

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,15 +1,14 @@
-import { Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
 
-  isSidebarToggled: boolean;
+  constructor() { }
 
-  toggleSidebar() {
-    this.isSidebarToggled = !this.isSidebarToggled;
-  }
+  ngOnInit() {}
+
 }
